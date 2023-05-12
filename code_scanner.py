bc46c3f41198d6dce408c19f2f697c00025902f9
Pulkit <123824584+PulkitRawat@users.noreply.github.com>
2023-05-12 13:37:42 +0530

    both codes can be scanned

--- code_scanner.py
+++ code_scanner.py
@@ -2,7 +2,7 @@ import qrcode
 import cv2 as cv
 from pyzbar.pyzbar import decode
 
-def read_qr_code(filepath):
+def read_code(filepath): 
     img = cv.imread(filepath)
     scan = decode(img)
     for obj in scan:
@@ -12,12 +12,15 @@ def read_qr_code(filepath):
     Data= obj.data
     return Data
 
+
+
+
 def create_qr_code():
     img=qrcode.make("https://www.youtube.com/")
     print(type(img))
     print(img.size)
     img.save('/qrcodec.png')
 
-create_qr_code()
-data = read_qr_code("/qrcodec.png")
+# create_qr_code()
+data = read_code("generatedBarcode.png")
 print(data)
